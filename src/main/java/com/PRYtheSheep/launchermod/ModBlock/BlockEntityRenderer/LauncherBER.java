4a0a155044c504ada76838dadadeff3dbf82e4eb
PRYtheSheep <peyruoyang521@gmail.com>
2024-03-05 00:02:22 +0800

    updated LauncherBER

--- src/main/java/com/PRYtheSheep/launchermod/ModBlock/BlockEntityRenderer/LauncherBER.java
+++ src/main/java/com/PRYtheSheep/launchermod/ModBlock/BlockEntityRenderer/LauncherBER.java
@@ -11,12 +11,15 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.fml.ISystemReportExtender;
 
 import static com.PRYtheSheep.launchermod.ModBlock.Launcher.Launcher.FACING;
 
 public class LauncherBER implements BlockEntityRenderer<LauncherBE> {
 
     BlockEntityRendererProvider.Context context;
+    static private double count = 0;
+    static private int flag = 0;
 
     public LauncherBER(BlockEntityRendererProvider.Context context) {
         this.context = context;
@@ -31,30 +34,90 @@ public class LauncherBER implements BlockEntityRenderer<LauncherBE> {
         BlockRenderDispatcher dispatcher = this.context.getBlockRenderDispatcher();
         Direction direction = pBlockEntity.getBlockState().getValue(FACING);
         BlockPos pos = pBlockEntity.getBlockPos();
-        PoseStack launcherStack = pPoseStack;
-        PoseStack barrelStack = pPoseStack;
 
-        //Set up the rotation of launcher_turret_launcher first
-        launcherStack.pushPose();
+        //Set up for launcher
+        //Only  manipulate and render the launcher here within push/pop pose
+        pPoseStack.pushPose();
         switch(direction){
-            case EAST -> launcherStack.rotateAround(Axis.YP.rotationDegrees(270), 0,0,0);
-            case WEST -> launcherStack.rotateAround(Axis.YP.rotationDegrees(90), 0,0,0);
-            case SOUTH -> launcherStack.rotateAround(Axis.YP.rotationDegrees(180), 0,0,0);
+            case EAST -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(270), 0,0,0);
+            case WEST -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(90), 0,0,0);
+            case SOUTH -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(180), 0,0,0);
         }
-        launcherStack.popPose();
 
-        //Set up the position of launcher_turret_launcher next
-        launcherStack.pushPose();
+        //Set up the position of launcher next
         switch(direction){
-            case NORTH -> launcherStack.translate(0.5,0.625,1.375);
+            case NORTH -> pPoseStack.translate(0.5,0.625,1.375);
+            case EAST -> pPoseStack.translate(0.5,0.625,0.375);
+            case WEST -> pPoseStack.translate(-0.5,0.625,1.375);
+            case SOUTH -> pPoseStack.translate(-0.5,0.625,0.375);
         }
-        dispatcher.renderSingleBlock(launcher, launcherStack, pBuffer, pPackedLight, pPackedOverlay);
-        launcherStack.popPose();
 
         //TESTING
-        barrelStack.pushPose();
-        barrelStack.rotateAround(Axis.YP.rotationDegrees(45), 0,0,0);
-        dispatcher.renderSingleBlock(barrel, barrelStack, pBuffer, pPackedLight, pPackedOverlay);
-        barrelStack.popPose();
+        pPoseStack.rotateAround(Axis.YP.rotationDegrees(27),0,0,0);
+        //TESTING
+
+        dispatcher.renderSingleBlock(launcher, pPoseStack, pBuffer, pPackedLight, pPackedOverlay);
+        pPoseStack.popPose();
+
+        //Set up for breach
+        //Only  manipulate and render the breach here within push/pop pose
+        pPoseStack.pushPose();
+        switch(direction){
+            case EAST -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(270), 0,0,0);
+            case WEST -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(90), 0,0,0);
+            case SOUTH -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(180), 0,0,0);
+        }
+
+        //Set up the position of breach next
+        switch(direction){
+            case NORTH -> pPoseStack.translate(0.5,0.625,1.375);
+            case EAST -> pPoseStack.translate(0.5,0.625,0.375);
+            case WEST -> pPoseStack.translate(-0.5,0.625,1.375);
+            case SOUTH -> pPoseStack.translate(-0.5,0.625,0.375);
+        }
+
+        //TESTING
+        pPoseStack.rotateAround(Axis.YP.rotationDegrees(27),0,0,0);
+        pPoseStack.rotateAround(Axis.XP.rotationDegrees(27),0,0.75F,0.75F);
+        //TESTING
+
+        dispatcher.renderSingleBlock(breach, pPoseStack, pBuffer, pPackedLight, pPackedOverlay);
+        pPoseStack.popPose();
+
+        //Set up for barrel
+        //Only  manipulate and render the barrel here within push/pop pose
+        pPoseStack.pushPose();
+        switch(direction){
+            case EAST -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(270), 0,0,0);
+            case WEST -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(90), 0,0,0);
+            case SOUTH -> pPoseStack.rotateAround(Axis.YP.rotationDegrees(180), 0,0,0);
+        }
+
+        //Set up the position of barrel next
+        switch(direction){
+            case NORTH -> pPoseStack.translate(0.5,0.625,1.375);
+            case EAST -> pPoseStack.translate(0.5,0.625,0.375);
+            case WEST -> pPoseStack.translate(-0.5,0.625,1.375);
+            case SOUTH -> pPoseStack.translate(-0.5,0.625,0.375);
+        }
+
+        //TESTING
+        pPoseStack.rotateAround(Axis.YP.rotationDegrees(27),0,0,0);
+        pPoseStack.rotateAround(Axis.XP.rotationDegrees(27),0,0.75F,0.75F);
+        if(flag==0){
+            count = count + 0.001;
+            pPoseStack.translate(0,0,count);
+        }
+        else if(flag==1){
+            count = count - 0.0002;
+            pPoseStack.translate(0,0,count);
+        }
+        if(count>=0.6) flag = 1;
+        if(count<=0) flag = 0;
+        System.out.println(count);
+        //TESTING
+
+        dispatcher.renderSingleBlock(barrel, pPoseStack, pBuffer, pPackedLight, pPackedOverlay);
+        pPoseStack.popPose();
     }
 }
